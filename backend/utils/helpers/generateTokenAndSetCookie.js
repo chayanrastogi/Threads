570e5b5d2c4fb34d5f42eb6b1ed344bdb2f8f30c
Chayan Rastogi <cp.chayanrastogi@gmail.com>
2024-01-05 02:43:50 +0530

    .

--- backend/utils/helpers/generateTokenAndSetCookie.js
+++ backend/utils/helpers/generateTokenAndSetCookie.js
@@ -10,7 +10,6 @@ const generateTokenAndSetCookie = (userId, res) => {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
 		sameSite: "strict", // CSRF
 		domain: "https://threads-server-zh1c.onrender.com", // Set the correct domain
-		secure: true,
 	});
 
 	return token;
