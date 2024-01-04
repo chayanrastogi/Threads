3eddff52c6ce60d545209faa9eb29cb2ba79557a
Chayan Rastogi <cp.chayanrastogi@gmail.com>
2024-01-04 20:45:46 +0530

    refactored cookie

--- backend/utils/helpers/generateTokenAndSetCookie.js
+++ backend/utils/helpers/generateTokenAndSetCookie.js
@@ -5,7 +5,7 @@ const generateTokenAndSetCookie = (userId, res) => {
 		expiresIn: "15d",
 	});
 
-	res.cookies("jwt", token, {
+	res.cookie("jwt", token, {
 		httpOnly: true, // more secure
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
 		sameSite: "strict", // CSRF
